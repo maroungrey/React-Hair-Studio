59a6996f8ee413f22eddb84dcc732dd85740c195
maroungrey <maroun.barqawi@gmail.com>
2023-09-09 13:30:27 -0700

    added booking

--- src/pages/BookPage.js
+++ src/pages/BookPage.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import { useEffect } from "react";
 import { Container, Row, Col } from 'react-bootstrap'
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 import AppNavbar from '../components/navbar';
 import AppFooter from '../components/footer';
 import DescriptionImg from '../assets/desc1.jpg'
+import Cal, { getCalApi } from "@calcom/embed-react";
 
 export default function BookPage() {
+
+  useEffect(()=>{
+	  (async function () {
+		const cal = await getCalApi();
+		cal("ui", {"theme":"light","styles":{"branding":{"brandColor":"#E6D9CC"}},"hideEventTypeDetails":false,"layout":"month_view"});
+	  })();
+	}, [])
+
   return (
     
     <div>
@@ -16,7 +26,11 @@ export default function BookPage() {
         <Container>
         <Row className=''>
         <h1 className='dancing-script my-5 text-center'>Make an Appointment</h1>
-                
+        <Cal
+          calLink="maroungrey/hair-salon-appointment"
+          style={{width:"100%",height:"100%",overflow:"scroll"}}
+          config={{layout: 'month_view'}}
+        />;
         </Row>
         </Container>
         <footer>
